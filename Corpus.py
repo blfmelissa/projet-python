120c361ab6ef9a76f96c5ebad1d7b5fbce6e2eb2
bouloufa <melissabouloufa@gmail.com>
2024-12-08 00:14:37 +0100

    td6

--- Corpus.py
+++ Corpus.py
@@ -1,7 +1,8 @@
 import Classes as cls
 from Classes import Author
 import re
-import pandas as pd 
+import pandas as pd
+from collections import defaultdict 
 
 class Corpus:
     def __init__(self, nom):
@@ -31,12 +32,87 @@ class Corpus:
             docs = list(sorted(docs, key=lambda x: x.date))[:n_docs]
         print("\n".join(list(map(repr, docs))))
 
-
     def __repr__(self):
         docs = list(self.id2doc.values())
         docs = list(sorted(docs, key=lambda x: x.titre.lower()))
 
         return "\n".join(list(map(str, docs)))
+    
+    def search(self, mot_cle) :
+        if not mot_cle or not isinstance(mot_cle, str):
+            raise ValueError("Le mot-clé est invalide")
+        try :
+            texte = self.textes_all
+        except : 
+            self.textes_all = " ".join([doc.texte for doc in [*self.id2doc.values()]])
+            texte = self.textes_all
+        pattern = re.compile(re.escape(mot_cle), re.IGNORECASE)
+        # re.escape garantit que les caractères spéciaux sont
+        # correctement interprétés comme du texte brut
+
+        res = pattern.finditer(texte)
+        if not res : 
+            print("Aucune occurrence trouvée.")
+            return []
+        
+        start_pattern = [m.start() for m in res]
+        print(f'{len(start_pattern)} occurrences trouvées')
+        return list(texte[i-20:i+20] for i in start_pattern)
+    
+    def concorde(self, mot_cle, taille) : 
+        try :
+            texte = self.textes_all
+        except : 
+            self.textes_all = " ".join([doc.texte for doc in [*self.id2doc.values()]])
+            texte = self.textes_all
+        pattern = re.compile(mot_cle, re.IGNORECASE)
+        res = pattern.finditer(texte)
+        pos_pattern = [m.span() for m in res]
+        context_left = pd.DataFrame("..."+texte[i-taille:i-1] for (i,j) in pos_pattern)
+        center = pd.DataFrame(texte[i:j] for (i,j) in pos_pattern)
+        context_right = pd.DataFrame(texte[j+1:j+taille]+"..." for (i,j) in pos_pattern)
+        result = pd.concat([context_left, center, context_right], axis = 1)
+        result.columns=["contexte gauche", "centre", "contexte droit"]
+        return result
+    
+    def nettoyer_texte(self, texte) : 
+        texte = texte.lower()
+        texte = texte.replace("\n"," ")
+        # Suppression des chiffres 
+        texte = re.sub(r'\d+', '', texte)
+        # Suppression de la ponctuation
+        texte = re.sub(r'[^\w\s]', '', texte)
+        # Suppression des espaces multiples
+        texte = re.sub(r'\s+', ' ', texte).strip()
+        return texte
+
+    def stats(self, nreturn=10) :
+        tf = defaultdict(int)  # occurrences des mots
+        df = defaultdict(int)    # nombre de documents contenant chaque mot
+        vocab = set()            
+        
+        for doc in self.id2doc.values():
+            texte_nettoye = self.nettoyer_texte(doc.get_text())
+            mots_doc = set(texte_nettoye.split())
+            vocab.update(mots_doc)
+            
+            for mot in mots_doc:
+                tf[mot] += texte_nettoye.split().count(mot)  
+                df[mot] += 1  
+    
+        tf_df = pd.DataFrame(list(tf.items()), columns=["Mot", "TF"])
+        df_df = pd.DataFrame(list(df.items()), columns=["Mot", "DF"])
+        # term frequency et document frequency
+        
+        # fusionner des deux dataframes sur la colonne "Mot"
+        result_df = pd.merge(tf_df, df_df, on="Mot")
+        
+        print(f"{len(vocab)} mots différents dans le vocabulaire")
+        print(f"Les {nreturn} mots les plus fréquents :")
+        print(result_df.nlargest(nreturn, 'TF')[['Mot', 'TF', 'DF']])
+        # tri par TF
+
+        return result_df
 
  
 # --------------- CLASSE USINE ----------------
@@ -51,4 +127,5 @@ class DocumentFactory:
             return cls.TheGuardianDocument(*args)
         else:
             raise ValueError("Source non supportée. Veuillez utiliser 'HackerNews' ou 'The_Guardian'.")
+    
     
