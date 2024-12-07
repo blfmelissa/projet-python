58c3184d9ddd6228cd17894ef950e096ce07a987
Pierrick Dennemont <pierrick.dennemont@gmail.com>
2024-12-07 21:04:06 +0100

    Version v1 du projet, branche Pierrick

---
+++ v1/Classes.py
@@ -0,0 +1,83 @@
+# =============== La classe Document ===============
+class Document:
+    # Initialisation des variables de la classe
+    def __init__(self, titre="", auteur="", date="", url="", texte=""):
+        self.titre = titre
+        self.auteur = auteur
+        self.date = date
+        self.url = url
+        self.texte = texte
+
+    def get_text(self) : 
+        return f"{self.texte}"
+    
+    def getType(self) :
+        pass
+
+    # Fonction qui renvoie le texte à afficher lorsqu'on tape repr(classe)
+    def __repr__(self):
+        return f"Titre : {self.titre}\tAuteur : {self.auteur}\tDate : {self.date}\tURL : {self.url}\tTexte : {self.texte}\t"
+
+    # Fonction qui renvoie le texte à afficher lorsqu'on tape str(classe)
+    def __str__(self):
+        return f"{self.titre}, par {self.auteur}"
+
+
+
+# =============== La classe Author ===============
+class Author:
+    def __init__(self, name):
+        self.name = name
+        self.ndoc = 0
+        self.production = []
+    #===== 2.5 : ADD ===============
+    def add(self, production):
+        self.ndoc += 1
+        self.production.append(production)
+
+    def __str__(self):
+        return f"Auteur : {self.name}\t# productions : {self.ndoc}"
+    
+
+
+
+# =============== La sous classe HackerNewsDocument ===============
+class HackerNewsDocument(Document) : 
+    def __init__(self, titre, auteur, date, url, texte, score) : 
+        Document.__init__(self, titre, auteur, date, url, texte)
+        self.setScore(score)
+
+    def getScore(self):
+        return self.score
+    
+    def setScore(self,score) : 
+        self.score = score
+
+    def __str__(self) : 
+        return f"L'article '{self.titre}' a été écrit par {self.auteur}"
+    
+    def getType(self) :
+        return "HackerNews"
+
+
+
+
+# =============== La sous classe HackerNewsDocument ===============
+class TheGuardianDocument(Document) : 
+    def __init__(self, titre, auteur, date, url, texte, firstPublicationDate) : 
+        Document.__init__(self, titre, auteur, date, url, texte)
+        self.setFirstPublicationDate(firstPublicationDate)
+
+    def getFirstPublicationDate(self):
+        return self.firstPublicationDate
+    
+    def setFirstPublicationDate(self,firstPublicationDate) : 
+        self.firstPublicationDate = firstPublicationDate
+
+    def __str__(self) : 
+        return f"L'article '{self.titre}' a été écrit par {self.auteur}"
+    
+    def getType(self) :
+        return "The_Guardian"
+
+
---
+++ v1/Corpus.py
@@ -0,0 +1,66 @@
+import Classes as cls
+from Classes import Author
+import re
+import pandas as pd 
+
+
+# =============== CLASSE CORPUS ===============
+
+class Corpus:
+    def __init__(self, nom):
+        self.nom = nom
+        self.authors = {}
+        self.aut2id = {}
+        self.id2doc = {}
+        self.ndoc = 0
+        self.naut = 0
+
+    def add(self, doc):
+        if doc.auteur not in self.aut2id:
+            self.naut += 1
+            self.authors[self.naut] = Author(doc.auteur)
+            self.aut2id[doc.auteur] = self.naut
+        self.authors[self.aut2id[doc.auteur]].add(doc.texte)
+
+        self.ndoc += 1
+        self.id2doc[self.ndoc] = doc
+
+    def show(self, n_docs=-1, tri="abc"):
+        #récupère les documents par leurs id dans la liste id2docs
+        docs = list(self.id2doc.values())
+        if tri == "abc":  # Tri alphabétique
+            docs = list(sorted(docs, key=lambda x: x.titre.lower()))[:n_docs]
+        elif tri == "123":  # Tri temporel
+            docs = list(sorted(docs, key=lambda x: x.date))[:n_docs]
+        print("\n".join(list(map(repr, docs))))
+
+
+    def __repr__(self):
+        docs = list(self.id2doc.values())
+        docs = list(sorted(docs, key=lambda x: x.titre.lower()))
+
+        return "\n".join(list(map(str, docs)))
+
+        
+
+
+
+
+# --------------- CLASSE USINE ----------------
+
+class DocumentFactory:
+    @staticmethod
+    def creerDoc(source, *args):
+        if source == 'HackerNews':
+            # Arguments attendus : titre, auteur, date, url, texte, nbCom
+            return cls.HackerNewsDocument(*args)
+        elif source == 'The_Guardian':
+            # Arguments attendus : titre, auteur, date, url, texte, coAuteur
+            return cls.TheGuardianDocument(*args)
+        else:
+            raise ValueError("Source non supportée. Veuillez utiliser 'HackerNews' ou 'The_Guardian'.")
+    
+
+
+
+
